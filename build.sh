6ccb492c840b7facb76fe33adc11e6b05e0e5706
Alon-98 <samtning@petalmail.com>
2025-05-26 21:43:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -55,8 +55,8 @@ pushd "${CONFIG_REPO}"
 
 git pull
 
-sed -i "'1i src-git ing https://github.com/xiealon/openwrt-package'; ${CONFIG_REPO}" feeds.conf.default
-sed -i "'2i src-git ing https://github.com/xiealon/small'; ${CONFIG_REPO}" feeds.conf.default
+sed -i "1i src-git ing https://github.com/xiealon/openwrt-package; ${CONFIG_REPO}" feeds.conf.default
+sed -i "2i src-git ing https://github.com/xiealon/small; ${CONFIG_REPO}" feeds.conf.default
 
 ./scripts/feeds update -a
 # if [ -d ./feeds/packages/lang/golang ]; then
